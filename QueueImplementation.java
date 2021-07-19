58aaacf198c1e719bd7b4fbf7f894fd5415f0562
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-19 11:49:44 +0530

    checked method of enque for adding element

---
+++ QueueImplementation.java
@@ -0,0 +1,13 @@
+package com.queue;
+
+public class QueueImplementation {
+	public static void main(String[] args) {
+		Queue<Integer> queue = new Queue<Integer>();
+		queue.enqueue(56);
+		queue.enqueue(30);
+		queue.enqueue(70);
+		queue.show();
+
+	}
+
+}

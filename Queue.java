1baaff87e9ee053de0bf6d22c96c33dc510e6d0a
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-19 11:48:57 +0530

    added enque method

---
+++ Queue.java
@@ -0,0 +1,23 @@
+package com.queue;
+
+public class Queue<T> {
+	LinkedList<T> queue = new LinkedList<>();
+
+	/**
+	 * It is use to add elements in the queue
+	 * 
+	 * @param data
+	 */
+	public void enqueue(T data) {
+		queue.add(data);
+		
+	}
+
+	/**
+	 * It prints all the elements present in the queue
+	 */
+	public void show() {
+		queue.show();
+	}
+
+}

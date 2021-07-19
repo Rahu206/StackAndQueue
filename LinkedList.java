f65e6a2d6d4bf7392f97531b6c2012bb7647dbca
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-19 12:13:12 +0530

    added linked list for removing element

---
+++ LinkedList.java
@@ -0,0 +1,101 @@
+package com.queue;
+
+public class LinkedList<T> {
+	Node<T> head;
+
+	// add elements to the linked list
+	
+	public void add(T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node<T> node = head;
+			while (node.next != null) {
+				node = node.next;
+			}
+			node.next = newNode;
+		}
+	}
+
+	/**
+	 * Inserts the element at the starting of the linked list
+	 * 
+	 * @param data
+	 */
+	public void addAtStart(T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+	}
+
+	/**
+	 * Current node should not be null and it is pointing to the next should be null
+	 */
+	public void show() {
+		Node<T> node = head;
+		while (node.next != null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		System.out.println(node.data);
+	}
+
+	/**
+	 * Inserts the specific element in between of two elements
+	 * 
+	 * @param index
+	 * @param data
+	 */
+	public void addAtIndex(int index, T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+
+		if (index == 0) {
+			addAtStart(data);
+		} else {
+			Node<T> node = head;
+			for (int i = 0; i < index - 1; i++) {
+				node = node.next;
+			}
+			newNode.next = node.next;
+			node.next = newNode;
+		}
+	}
+	
+
+	/**
+	 * It removes the last element present in the Linked List
+	 */
+	public void popLast() {
+		Node<T> node = head;
+		Node<T> secondlastNode = head;
+		while (node.next != null) {
+			secondlastNode = node;
+			node = node.next;
+		}
+		secondlastNode.next = null;
+	}
+
+	
+	/**
+	 * It removes the top most element from queue and stack
+	 * @return
+	 */
+	public int pop() {
+        if (head != null) {
+            int size = 0;
+            int temp = (int) head.data;
+            head = head.next;
+            size--;
+            return temp;
+        } else {
+            System.out.println("Empty list");
+            return -1;
+        }
+    }
+	
+
+}
